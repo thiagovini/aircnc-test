b70f58c4ee07f92775d37038e65021d546ab438b
Thiagovini <vincius.thiago021@gmail.com>
2020-09-08 02:31:09 -0300

    Finishing backend

--- backend/src/config/upload.js
+++ backend/src/config/upload.js
@@ -1,11 +1,16 @@
 const multer = require('multer');
-const path = require( 'path')
+const path = require( 'path');
+const { findOne } = require('../models/User');
 
 module.exports = {
-    storege : multer.diskStorage({
-        destination : path.resolve(__dirname, '..', '..', 'uploads'),
+    storage: multer.diskStorage({
+        destination: path.resolve(__dirname, '..', '..', 'uploads'),
         filename: (req, file, cb) => {
-            cb(null, `${file.fieldname}-${Date.now}${path.extname(file.originalname)}`)
-        }
-    })
+
+            const ext = path.extname(file.originalname);
+            const name = path.basename(file.originalname, ext);
+
+            cb(null, `${name}-${Date.now()}${ext}`);
+        },
+    }),
 }
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const SessionControlle = require('./controllers/SessionController');
 const SpotController = require('./controllers/SpotController');
+const ProfileController = require('./controllers/profileController');
+const BookingController = require('./controllers/BookingController');
 
 const multer = require('multer');
 const uploadConfig = require('./config/upload');
@@ -9,6 +11,10 @@ const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionControlle.store);
+
+routes.get('/spots', SpotController.index);
 routes.post('/spots', upload.single('thumbnail'), SpotController.store);
+routes.get('/profileController', ProfileController.show);
+routes.post('/spots/:spot_id/bookings', BookingController.store);
 
 module.exports = routes;
